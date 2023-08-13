7356b55aeeff69407255fdba2ee53fbd00f238a8
jburky15 <jburky15@gmail.com>
2023-08-13 17:49:52 -0400

    Made the events editable after initial submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,38 @@ const baseUrl = "http://localhost:5000"
 
 function App() {
   const [description, setDescription] = useState("");
-  const [eventsList, setEventsList] = useState([])
+  const [editDescription, setEditDescription] = useState("");
+  const [eventsList, setEventsList] = useState([]);
+  const [eventId, setEventId] = useState(null);
 
-  const handleChange = e => {
-    setDescription(e.target.value);
+  const handleChange = (e, field) => {
+    if (field === 'edit') {
+      setEditDescription(e.target.value)
+    } else {
+      setDescription(e.target.value)
+    }
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const data = await axios.post(`${ baseUrl }/events`, { description })
-    setEventsList([...eventsList, data.data])
-    setDescription('')
+      if (editDescription) {
+        const data = await axios.put(`${ baseUrl }/events/${ eventId }`, { description: editDescription })
+        const updatedEvent = data.data.event
+        const updatedList = eventsList.map(event => {
+          if (event.id === eventId) {
+            return event = updatedEvent
+          }
+          return event
+        })
+        setEventsList(updatedList)
+      } else {
+        const data = await axios.post(`${ baseUrl }/events`, { description })
+        setEventsList([...eventsList, data.data])
+      }
+      setDescription('')
+      setEditDescription('')
+      setEventId(null)
     } catch(err) {
       console.error(err.message)
     }
@@ -32,6 +52,11 @@ function App() {
     } catch (err) {
       console.log(err.message)
     }
+  }
+
+  const toggleEdit = (event) => {
+    setEventId(event.id)
+    setEditDescription(event.description)
   }
 
   const fetchEvents = async () => {
@@ -50,20 +75,48 @@ function App() {
       <section id="eventInput">
         <form onSubmit={ handleSubmit }>
           <label htmlFor="description">What the baby do?</label>
-          <input onChange={ handleChange } type="text" name="description" id="descriptionInput" value={ description } placeholder='Baby Event' />
-          <input type="submit" value="SUBMIT" className='submitBtn' />
+          <input 
+            onChange={ e => handleChange(e, 'description') } 
+            type="text" 
+            name="description" 
+            id="descriptionInput" 
+            value={ description } 
+            placeholder='Baby Event'
+          />
+          <input 
+            type="submit" 
+            value="SUBMIT" 
+            className='submitBtn' 
+          />
         </form>
       </section>
 
       <section id="eventsList">
         <ul>
           {eventsList.map(event => {
-            return (
+            if (eventId === event.id) {
+              return (
+                <li>
+                  <form onSubmit={ handleSubmit } key={ event.id }>
+                    <input onChange={ (e) => handleChange(e, 'edit') } 
+                      type="text" 
+                      name="editDescription" 
+                      id="editDescription" 
+                      value={ editDescription }
+                    />
+                    <button type='submit'>Submit</button>
+                  </form>
+                </li>
+              )
+            } else {
+              return (
                 <li key={event.id}>
-                  {event.description}
+                  { event.description }
+                  <button onClick={() => toggleEdit(event)}>Edit</button>
                   <button className="delBtn" onClick={() => handleDelete(event.id)}>Delete</button>
                 </li>
             )
+            }
           })}
         </ul>
       </section>
